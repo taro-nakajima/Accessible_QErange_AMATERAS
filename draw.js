af8d7467596b312b3ce398245bd70c64a2776f9a
Taro Nakajima <nakajima.tarou@gmail.com>
2019-11-07 15:34:59 +0900

    update 20191107
    chopper modes are revised

--- draw.js
+++ draw.js
@@ -1,9 +1,8 @@
-//JavaScript code for calculating accessible Q-E ranges for HRC
+//JavaScript code for calculating accessible Q-E ranges for AMATERAS
 // programed by T. Nakajima (ISSP-NSL) Oct. 20, 2019.
 var Ei_numMax=5;
 var Ei = new Array(Ei_numMax);
 var decimal_digit = 1000;
-var isOptimumEi= new Array(Ei_numMax);
 
 
 function draw() {
@@ -20,15 +19,15 @@ function draw_TOF(){
     var marginX = 50;
     var marginY = 20;
 
-    var TOFscale = 10.0;    // ms to pixel
+    var TOFscale = 6.0;    // ms to pixel
     var Lscale=10.0;        // meter to pixel
 
-    var Ltotal_R = 19.0;      // Real source to detector (m)
-    var Lsc_R = 13.97;        // Real sample chopper distance  (m)
-    var L1_R = 15.0;          // Real source to sample distance (m)
-    var TOF_len_R = 40;       // Real TOF max (ms)
+    var Ltotal_R = 34.0;      // Real source to detector (m)
+    var Lsc_R = 28.4;        // Real sample chopper distance  (m)
+    var L1_R = 30.0;          // Real source to sample distance (m)
+    var TOF_len_R = 80;       // Real TOF max (ms)
     var TOFconst = 2.286;       // TOF at 1 m is 2.286/sqrt(E)
-    var upperLimitEi = 8000;    // upper limit of Ei 8eV
+    var upperLimitEi = 80;    // upper limit of Ei 8eV
 
     var Ltotal=Ltotal_R*Lscale;
     var Lsc = Lsc_R*Lscale;
@@ -47,48 +46,31 @@ function draw_TOF(){
     var canvas2 = document.getElementById('CanvasTOF');
     var context2 = canvas2.getContext('2d');
 
-    var chopperFace = Boolean(Number(document.getElementById('chopperFace').value));
 
     var freq = Number(document.getElementById('freq').value);
-    var ChopPeriod_R = 1.0/freq*1000.0/2;       //Real chopper period (ms). A factor "1/2" is necessary for Fermi chopper
+    var ChopPeriod_R = 1.0/freq*1000.0;
     var ChopPeriod = ChopPeriod_R*TOFscale;
     var ChopRept = TOF_len_R/ChopPeriod_R;
 
     var TargetEi = Number(document.getElementById('targetEi').value);
     var TargetTOF_at_Chopper=(TOFconst*(Lsc_R)/Math.sqrt(TargetEi));
 
+    var upperLimitEi = Number(document.getElementById('upperLimitEi').value);
+
+
     var ChopOfst_R =0;      //Real chopper offset (ms)
 
-    var isOptimumWindow = new Array(ChopRept);
 
-    for (var tt=0;tt<=ChopRept;tt+=2){
+    for (var tt=0;tt<=ChopRept;tt+=1){
         var t1=(tt)*ChopPeriod_R;
-        var t2=(tt+1.0)*ChopPeriod_R;
-        var t3=(tt+2.0)*ChopPeriod_R;
+        var t3=(tt+1.0)*ChopPeriod_R;
 
-        if ((TargetTOF_at_Chopper > t1) && (TargetTOF_at_Chopper <= t2) ){
+        if ((TargetTOF_at_Chopper >= t1) && (TargetTOF_at_Chopper < t3) ){
             ChopOfst_R=TargetTOF_at_Chopper-t1;
-            for (var uu=0;uu<ChopRept;uu+=2){
-                isOptimumWindow[uu]=true;
-                isOptimumWindow[uu+1]=false;
-            }
-        }
-        else if((TargetTOF_at_Chopper > t2) && (TargetTOF_at_Chopper <= t3) ){
-            ChopOfst_R=TargetTOF_at_Chopper-t3;
-            for (var uu=0;uu<ChopRept;uu+=2){
-                isOptimumWindow[uu]=false;
-                isOptimumWindow[uu+1]=true;
-            }
         }
     }
 
     var displayChopperOfst = ChopOfst_R;
-    if (chopperFace == true){
-        displayChopperOfst +=0;
-    }
-    else {
-        displayChopperOfst += ChopPeriod_R;       // Another half rotation is necessary to have optimum condition for the target Ei
-    }
 
     if(displayChopperOfst<0){
         displayChopperOfst += ChopPeriod_R*2.0;
@@ -99,12 +81,6 @@ function draw_TOF(){
 
     document.getElementById('offset').value=Math.round(displayChopperOfst*decimal_digit)/decimal_digit;
 
-//    if(ChopOfst_R>0){
-//        document.getElementById('offset').value=Math.round(ChopOfst_R*decimal_digit)/decimal_digit;
-//    }
-//    else {
-//        document.getElementById('offset').value=Math.round((ChopOfst_R+ChopPeriod_R*2.0)*decimal_digit)/decimal_digit;        // Display value of chopper offset should be positive. 
-//    }
     var ChopOfst = ChopOfst_R*TOFscale;
 
     var temp = document.getElementById('Ei_Num_ofst');
@@ -116,6 +92,9 @@ function draw_TOF(){
     context2.strokeStyle = "rgb(0, 0, 0)";
     context2.lineWidth=1;
 
+    //for debug
+//    context2.fillText(ChopPeriod_R, marginX,marginY);
+//    context2.fillText(TargetTOF_at_Chopper, marginX,marginY);
 
     //text labels
     context2.font = "italic 10px sans-serif";
@@ -133,12 +112,12 @@ function draw_TOF(){
 
     // x ticks
     context2.font = " 10px sans-serif";
-    for (var i=0;i<5;i+=1){
+    for (var i=0;i<9;i+=1){
         context2.beginPath();
-        context2.moveTo(marginX+TOF_len/4*i, marginY+Ltotal);
-        context2.lineTo(marginX+TOF_len/4*i, marginY+Ltotal-TickL);
+        context2.moveTo(marginX+TOF_len/8*i, marginY+Ltotal);
+        context2.lineTo(marginX+TOF_len/8*i, marginY+Ltotal-TickL);
         context2.stroke();
-        context2.fillText(i*10, marginX+TOF_len/4*i-TextSize/2, marginY+Ltotal+TextSize*1.5);
+        context2.fillText(i*10, marginX+TOF_len/8*i-TextSize/2, marginY+Ltotal+TextSize*1.5);
     }
 
     // x label
@@ -170,32 +149,16 @@ function draw_TOF(){
     context2.strokeStyle = "rgb(100, 100, 100)";
     context2.beginPath();
     context2.moveTo(marginX, Ltotal+marginY-Lsc);
-    if(isOptimumWindow[0]==true){       //ChopOfst >0
-        context2.lineTo(marginX-ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
-        context2.stroke();
-        TOF_at_Chopper[0]=(ChopOfst)/10.0;    
-    }
-    else {      //ChopOfst<0
-        context2.lineTo(marginX+ChopPeriod-ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
-        context2.stroke();
-        TOF_at_Chopper[0]=(ChopPeriod+ChopOfst)/10.0;    
-    }
+    context2.lineTo(marginX-ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
+    context2.stroke();
+    TOF_at_Chopper[0]=(ChopOfst_R);    
 
-    for (var i = 1; i < ChopRept; i += 1) {
-        if(isOptimumWindow[0]==true){
-            context2.beginPath();
-            context2.moveTo(marginX+ChopPeriod*(i-1)+ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
-            context2.lineTo(marginX+ChopPeriod*(i)-ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
-            context2.stroke();
-            TOF_at_Chopper[i]=(ChopPeriod*(i)+ChopOfst)/10.0;    
-        }
-        else{
-            context2.beginPath();
-            context2.moveTo(marginX+ChopPeriod*(i)+ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
-            context2.lineTo(marginX+ChopPeriod*(i+1)-ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
-            context2.stroke();
-            TOF_at_Chopper[i]=(ChopPeriod*(i+1)+ChopOfst)/10.0;    
-        }
+    for (var i = 1; i < ChopRept+1; i += 1) {
+        context2.beginPath();
+        context2.moveTo(marginX+ChopPeriod*(i-1)+ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
+        context2.lineTo(marginX+ChopPeriod*(i)-ChopperOpen/2+ChopOfst, Ltotal+marginY-Lsc);
+        context2.stroke();
+        TOF_at_Chopper[i]=(ChopPeriod_R*(i)+ChopOfst_R);    
     }
 
     // Determine Ei num offset
@@ -212,31 +175,16 @@ function draw_TOF(){
         var idE='E'+(i+1);
         document.getElementById(idE).value = Math.round((TOFconst/TOF_at_Chopper[Ei_num_ofst+i]*(Lsc_R))**2.0*decimal_digit)/decimal_digit ;
         Ei[i]=(TOFconst/TOF_at_Chopper[Ei_num_ofst+i]*(Lsc_R))**2.0 ;
-        isOptimumEi[i]=isOptimumWindow[Ei_num_ofst+i];
-        if (isOptimumEi[i]==true){
-            var idIsOptium ='isE'+(i+1)+'Optimum';
-            document.getElementById(idIsOptium).innerHTML = '(Optimum)' ;
-        }
-        else {
-            var idIsOptium ='isE'+(i+1)+'Optimum';
-            document.getElementById(idIsOptium).innerHTML = '' ;
-        }
     }
 
     context2.lineWidth=1;
     for (var i = 0; i < Ei_numMax; i += 1) {
-        if (isOptimumEi[i]==true){
-            context2.strokeStyle = "rgb(255, 0, 0)";
-            context2.lineWidth=2;
-        }
-        else {
-            context2.strokeStyle = "rgb(255, 150, 150)";
-            context2.lineWidth=1;
-        }
+        context2.strokeStyle = "rgb(255, 0, 0)";
+        context2.lineWidth=2;
         context2.beginPath();
         context2.moveTo(marginX, marginY+Ltotal);
 //        context2.lineTo(marginX+TOF_at_Chopper[Ei_num_ofst+i]*10.0*Ltotal/Lsc, marginY);
-        context2.lineTo(marginX+TOF_at_Chopper[Ei_num_ofst+i]*10.0*Ltotal/Lsc, marginY);
+        context2.lineTo(marginX+TOF_at_Chopper[Ei_num_ofst+i]*TOFscale*Ltotal/Lsc, marginY);
         context2.stroke();
     }
     context2.lineWidth=1;
@@ -283,17 +231,30 @@ function draw_Qxy(){
     qh[2] = Number(document.getElementById('qh3').value);
     qk[2] = Number(document.getElementById('qk3').value);
 
+    var DetBankNum = 10;
 
-    var tth_Max = new Array(4);
-    var tth_Min = new Array(4);
-    tth_Max[0] = Number(document.getElementById('D1_tth_max').value);
-    tth_Min[0] = Number(document.getElementById('D1_tth_min').value);
-    tth_Max[1] = Number(document.getElementById('D2_tth_max').value);
-    tth_Min[1] = Number(document.getElementById('D2_tth_min').value);
-    tth_Max[2] = Number(document.getElementById('D3_tth_max').value);
-    tth_Min[2] = Number(document.getElementById('D3_tth_min').value);
-    tth_Max[3] = Number(document.getElementById('D4_tth_max').value);
-    tth_Min[3] = Number(document.getElementById('D4_tth_min').value);
+    var tth_Max = new Array(DetBankNum);
+    var tth_Min = new Array(DetBankNum);
+    tth_Max[0] = Number(document.getElementById('D11_tth_max').value);
+    tth_Min[0] = Number(document.getElementById('D11_tth_min').value);
+    tth_Max[1] = Number(document.getElementById('D10_tth_max').value);
+    tth_Min[1] = Number(document.getElementById('D10_tth_min').value);
+    tth_Max[2] = Number(document.getElementById('D9_tth_max').value);
+    tth_Min[2] = Number(document.getElementById('D9_tth_min').value);
+    tth_Max[3] = Number(document.getElementById('D8_tth_max').value);
+    tth_Min[3] = Number(document.getElementById('D8_tth_min').value);
+    tth_Max[4] = Number(document.getElementById('D7_tth_max').value);
+    tth_Min[4] = Number(document.getElementById('D7_tth_min').value);
+    tth_Max[5] = Number(document.getElementById('D6_tth_max').value);
+    tth_Min[5] = Number(document.getElementById('D6_tth_min').value);
+    tth_Max[6] = Number(document.getElementById('D5_tth_max').value);
+    tth_Min[6] = Number(document.getElementById('D5_tth_min').value);
+    tth_Max[7] = Number(document.getElementById('D4_tth_max').value);
+    tth_Min[7] = Number(document.getElementById('D4_tth_min').value);
+    tth_Max[8] = Number(document.getElementById('D3_tth_max').value);
+    tth_Min[8] = Number(document.getElementById('D3_tth_min').value);
+    tth_Max[9] = Number(document.getElementById('D2_tth_max').value);
+    tth_Min[9] = Number(document.getElementById('D2_tth_min').value);
 
 
     var radius = 3; // radius for each reciprocal lattice point
@@ -301,9 +262,8 @@ function draw_Qxy(){
     var originX = canvas3[0].width/2.0;
     var originY = canvas3[0].height/2.0;
 
-    var DetBankNum = 4;
 
-    var scale0 = 1.5;
+    var scale0 = 1.0;       // 2ki = canvas.width/2 when scale0=1.0
 
     var scale = new Array(5);
     var ki = new Array(5);
@@ -349,7 +309,7 @@ function draw_Qxy(){
         context3[p].beginPath();
         context3[p].lineWidth=1;
 
-        var dX=(Math.cos(Math.PI/180.0*tth_Min[i_tth])*kf-1.0*ki[p])*scale[p];
+        var dX=-(Math.cos(Math.PI/180.0*tth_Min[i_tth])*kf-1.0*ki[p])*scale[p];
         var dY=(Math.sin(Math.PI/180.0*tth_Min[i_tth]))*kf*scale[p];
 
         var tempX = cosOmg1*dX - sinOmg1*dY;
@@ -361,7 +321,7 @@ function draw_Qxy(){
 
         context3[p].moveTo(originX+dX, originY-dY);
         for (var tth= tth_Min[i_tth]; tth <= tth_Max[i_tth]; tth += 0.5) {
-            var dX=(Math.cos(Math.PI/180.0*tth)*kf-1.0*ki[p])*scale[p];
+            var dX=-(Math.cos(Math.PI/180.0*tth)*kf-1.0*ki[p])*scale[p];
             var dY=(Math.sin(Math.PI/180.0*tth))*kf*scale[p];
 
             var tempX = cosOmg1*dX - sinOmg1*dY;
@@ -373,7 +333,7 @@ function draw_Qxy(){
             context3[p].lineTo(originX+dX , originY - dY);
         }
         for (var omg= omg1; omg < omg2; omg += 0.5) {
-            var dX=(Math.cos(Math.PI/180.0*tth_Max[i_tth])*kf-1.0*ki[p])*scale[p];
+            var dX=-(Math.cos(Math.PI/180.0*tth_Max[i_tth])*kf-1.0*ki[p])*scale[p];
             var dY=(Math.sin(Math.PI/180.0*tth_Max[i_tth]))*kf*scale[p];
 
             var tempX = Math.cos(Math.PI/180.0*omg)*dX - Math.sin(Math.PI/180.0*omg)*dY;
@@ -385,7 +345,7 @@ function draw_Qxy(){
             context3[p].lineTo(originX+dX , originY - dY);
         }
         for (var i= tth_Max[i_tth]; i >= tth_Min[i_tth]; i -= 0.5) {
-            var dX=(Math.cos(Math.PI/180.0*i)*kf-1.0*ki[p])*scale[p];
+            var dX=-(Math.cos(Math.PI/180.0*i)*kf-1.0*ki[p])*scale[p];
             var dY=(Math.sin(Math.PI/180.0*i))*kf*scale[p];
 
             var tempX = cosOmg2*dX - sinOmg2*dY;
@@ -396,7 +356,7 @@ function draw_Qxy(){
             context3[p].lineTo(originX+dX , originY - dY);
         }
         for (var omg= omg2; omg > omg1; omg -= 0.5) {
-            var dX=(Math.cos(Math.PI/180.0*tth_Min[i_tth])*kf-1.0*ki[p])*scale[p];
+            var dX=-(Math.cos(Math.PI/180.0*tth_Min[i_tth])*kf-1.0*ki[p])*scale[p];
             var dY=(Math.sin(Math.PI/180.0*tth_Min[i_tth]))*kf*scale[p];
 
             var tempX = Math.cos(Math.PI/180.0*omg)*dX - Math.sin(Math.PI/180.0*omg)*dY;
@@ -511,7 +471,7 @@ function draw_Qxy(){
     context3[p].strokeStyle="rgb(250, 100, 100)";
     context3[p].lineWidth=2;
     context3[p].moveTo(originX,originY);
-    context3[p].lineTo(originX-a_star*scale[p] , originY);
+    context3[p].lineTo(originX+a_star*scale[p] , originY);
     context3[p].stroke();
 
     var arrow_head = 12;
@@ -520,19 +480,19 @@ function draw_Qxy(){
     context3[p].lineWidth=1;
     context3[p].fillStyle="rgb(250, 100, 100)";
     //context3[p].moveTo(originX-a_star*scale[p] , originY);
-    context3[p].moveTo(originX-a_star*scale[p] , originY-arrow_head/2);
-    context3[p].lineTo(originX-a_star*scale[p]-arrow_head*0.7 , originY);
-    context3[p].lineTo(originX-a_star*scale[p] , originY+arrow_head/2);
+    context3[p].moveTo(originX+a_star*scale[p] , originY-arrow_head/2);
+    context3[p].lineTo(originX+a_star*scale[p]+arrow_head*0.7 , originY);
+    context3[p].lineTo(originX+a_star*scale[p] , originY+arrow_head/2);
     context3[p].fill();
     context3[p].font = "italic 14px sans-serif";
-    context3[p].fillText("a*", originX-a_star*scale[p]/2-font_size , originY-font_size/3 )
+    context3[p].fillText("a*", originX+a_star*scale[p]/2+font_size , originY-font_size/3 )
 
     // draw b star
     context3[p].beginPath();
     context3[p].strokeStyle="rgb(250, 100, 100)";
     context3[p].lineWidth=2;
     context3[p].moveTo(originX,originY);
-    context3[p].lineTo(originX-b_star*cosGamma*scale[p], originY+b_star*sinGamma*scale[p]);
+    context3[p].lineTo(originX+b_star*cosGamma*scale[p], originY-b_star*sinGamma*scale[p]);
     context3[p].stroke();
 
     context3[p].beginPath();
@@ -541,14 +501,14 @@ function draw_Qxy(){
 
     var arrow_head_X = Array(3);
     var arrow_head_Y = Array(3);
-    arrow_head_X[0]=-b_star*scale[p];
-    arrow_head_Y[0]=-arrow_head/2.0;
+    arrow_head_X[0]=b_star*scale[p];
+    arrow_head_Y[0]=arrow_head/2.0;
 
-    arrow_head_X[1]=-b_star*scale[p]-arrow_head*0.7;
+    arrow_head_X[1]=b_star*scale[p]+arrow_head*0.7;
     arrow_head_Y[1]=0.0;
 
-    arrow_head_X[2]=-b_star*scale[p];
-    arrow_head_Y[2]=arrow_head/2.0;
+    arrow_head_X[2]=+b_star*scale[p];
+    arrow_head_Y[2]=-arrow_head/2.0;
 
     for (var l=0;l<3;l+=1){
         var tempX=cosGamma*arrow_head_X[l]-sinGamma*arrow_head_Y[l];
@@ -562,7 +522,7 @@ function draw_Qxy(){
     context3[p].lineTo(originX+arrow_head_X[2] , originY-arrow_head_Y[2]);
     context3[p].fill();
     context3[p].font = "italic 14px sans-serif";
-    context3[p].fillText("b*", originX+arrow_head_X[1]/2-font_size*1.4, originY-arrow_head_Y[1]/2+font_size );
+    context3[p].fillText("b*", originX+arrow_head_X[1]/2-font_size*1.4, originY-arrow_head_Y[1]/2-font_size );
 
 
 
